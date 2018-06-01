76e0f96366603cf8e4dca66b5085827cbebd1b41
staronline1985 <staronline1985@gmail.com>
2018-06-01 12:59:22 +0530

    Update users.js
    commit changes

--- imports/ui/pages/admin/userslist/users.js
+++ imports/ui/pages/admin/userslist/users.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { PropTypes } from 'react'
 import { Meteor } from 'meteor/meteor'
 
@@ -13,37 +13,13 @@ const UserList = (users, isReady) => {
         )
 
     }
-=======
-import React,{PropTypes} from 'react'
-import { Meteor } from 'meteor/meteor'
-
-const UserList = (props) => {
-
-   if(!props.isReady) { (<ul>
-        {
-            props.users.map((u) => {
-
-                <li>{u.name}</li>
-
-            })
-        }
-    </ul>)
-   }
->>>>>>> c4e54a3a53ff0ba6c3b36cf21a95ad8b2c60b531
-
-
 };
 
 
 
 UserList.propTypes = {
-
-<<<<<<< HEAD
     users: PropTypes.array,
     isReady: PropTypes.bool
-=======
-    users: PropTypes.array
->>>>>>> c4e54a3a53ff0ba6c3b36cf21a95ad8b2c60b531
 
 }
 
